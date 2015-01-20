cbfd4797a93b42d2c2747a0382143591345092f6
rainymoon911 <zhangrain911@163.com>
2015-01-20 19:52:19 +0800

    add xblock code

---
+++ document_by_zyu/edx-xblock-codebrowser/static/js/src/codebrowser_edit.js
@@ -0,0 +1,16 @@
+function CodeBrowserEditBlock(runtime, element) {
+  $(element).find('.save-button').bind('click', function() {
+    var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
+    var data = {
+      width: $(element).find('input[name=width]').val(),
+      height: $(element).find('input[name=height]').val(),
+    };
+    $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
+      window.location.reload(false);
+    });
+  });
+
+  $(element).find('.cancel-button').bind('click', function() {
+    runtime.notify('cancel', {});
+  });
+}
