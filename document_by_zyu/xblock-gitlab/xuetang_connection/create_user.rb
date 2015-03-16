f4cfba21c86e12d788ea6c4ca72c6346b903a8e6
rainymoon911 <zhangrain911@163.com>
2015-03-16 22:48:01 +0800

    Create create_user.rb

---
+++ document_by_zyu/xblock-gitlab/xuetang_connection/create_user.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/ruby
+require 'uri'
+require 'net/http'
+email = ARGV[0]
+password = ARGV[1]
+username = ARGV[2]
+name = ARGV[3]
+uri = URI.parse("http://south.cs.tsinghua.edu.cn/api/v3/users")
+http = Net::HTTP::new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.request_uri)
+request.set_form_data({"username" => username, "email" => email, "password" => password, "name" => name})
+request["PRIVATE-TOKEN"] = "Gysy4D6M1-B6sTNtyu2S"
+response = http.request(request)
+puts "#{response.body}"
